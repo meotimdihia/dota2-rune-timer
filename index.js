b7b940fe0b81d2b97f2422ad5d8480f3288f33c3
Dien Vu <meotimdihia@gmail.com>
2019-08-01 17:32:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fastify = require('fastify')({})
 
 async function beep(loop = 2) {
   for (let i = 0; i < loop; i++) {
-	  process.stderr.write("\007")
+		process.stderr.write("\007")
 	  await new Promise(resolve => setTimeout(resolve, 500))
   }
 }
@@ -17,10 +17,9 @@ fastify.post('/', (request, reply) => {
 	if (request.body && request.body.map && request.body.map.clock_time) {
 		
 		const time = Number(request.body.map.clock_time)
+		const date = new Date()
 		
 		// alert for rune on the river
-		const date = new Date()
-
 		if (date.getTime() - alerted > 30000 && (Math.round(time / 60) % 2 == 0) && (time % 60 > 30) && (time % 60 < 45)) {
 			alerted = date.getTime()
 			console.log("Check runes on the river!")
@@ -49,14 +48,13 @@ const start = async () => {
   try {
     await fastify.listen(3000)
     console.log(`Server listening on ${fastify.server.address().port}`)
-	console.log(`\n`)
-	console.log(`Please copy file: "gamestate_integration_runetimer.cfg" to "{steam_directory}\steamapps\common\dota 2 beta\game\dota\cfg\gamestate_integration\gamestate_integration_runetimer.cfg"`)
-	console.log(`\n`)
-	console.log(`The default steam path is "C:\Program Files (x86)\Steam\steamapps\common\dota 2 beta\game\dota\cfg\gamestate_integration\gamestate_integration_runetimer.cfg"`)
-	console.log(`\n`)
-	console.log(`When time for runes is comming, the program alert system beep`)
-	console.log(`Beep should be alert 2 times for runes on river, and 3 times for bounty runes`)
-	
+		console.log(`\n`)
+		console.log(`Please copy file: "gamestate_integration_runetimer.cfg" to "{steam_directory}\steamapps\common\dota 2 beta\game\dota\cfg\gamestate_integration\gamestate_integration_runetimer.cfg"`)
+		console.log(`\n`)
+		console.log(`The default steam path is "C:\Program Files (x86)\Steam\steamapps\common\dota 2 beta\game\dota\cfg\gamestate_integration\gamestate_integration_runetimer.cfg"`)
+		console.log(`\n`)
+		console.log(`When time for runes is comming, the program alert system beep`)
+		console.log(`Beep should be alert 2 times for runes on river, and 3 times for bounty runes`)
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
